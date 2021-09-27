965289f3c14528d9d2c6b70213a3452d1184f415
Samil Abud <samilabud@gmail.com>
2021-09-27 14:32:45 -0400

    heroku fixing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
+const path = require('path');
 //local modules
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
